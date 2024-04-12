92c48a03cb30fb51cfbfeaebe47a08934ddc6b17
이강준 <47883234+Ladun@users.noreply.github.com>
2024-04-12 18:04:26 +0900

    feat: Default ppo update

---
+++ test.bat
@@ -0,0 +1,2 @@
+
+python main.py --train --config=configs/Ant-v4.yaml
--- utils/stuff.py
+++ utils/stuff.py
@@ -2,99 +2,89 @@ import os
 import numpy as np
 import torch
 
+def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, batch_count):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
 
-class RunningStat:
-    """
-    https://www.johndcook.com/blog/standard_deviation/
-    """
-    def __init__(self):
-        self.__m_n = 0
-        self.__m_oldM = 0
-        self.__m_newM = 0
-        self.__m_oldS = 0
-        self.__m_newS = 0
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
 
-    def reset(self):
-        self.__m_n = 0
+    return new_mean, new_var, new_count
 
-    def push(self, x):
-        self.__m_n += 1
 
-        if self.__m_n == 1:
-            self.__m_oldM = x
-            self.__m_newM = x
-            self.__m_oldS = 0.
-        else:
-            self.__m_newM = self.__m_oldM + (x - self.__m_oldM) / self.__m_n
-            self.__m_newS = self.__m_oldS + (x - self.__m_oldM) * (x - self.__m_newM)
+class RunningMeanStd:
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
 
-            # set up for next iteration
-            self.__m_oldM = self.__m_newM
-            self.__m_oldS = self.__m_newS
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
 
-    def get_num_data_values(self):
-        return self.__m_n
-
-    def mean(self):
-        if self.__m_n > 0:
-            return self.__m_newM
-        else:
-            return 0
-
-    def variance(self):
-        if self.__m_n > 1:
-            return self.__m_newS / (self.__m_n - 1)
-        else:
-            return 0.
-
-    def std(self):
-        return np.sqrt(self.variance())
-
-    def load_variables(self, saved_dict):
-        self.__m_n = saved_dict['n']
-        self.__m_oldM = saved_dict['mu']
-        self.__m_newM = saved_dict['mu']
-        self.__m_oldS = saved_dict['var']
-        self.__m_newS = saved_dict['var']
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
 
     def save_variables(self):
-        return {"n": self.__m_n,
-                "mu": self.__m_newM,
-                "var": self.__m_newS}
+        return {
+            "mean": self.mean,
+            "var": self.var,
+            "count": self.count
+        }
 
-class RewardScaler:
-    def __init__(self, gamma=1.):
-        self.rs = RunningStat()
-        self.gamma = gamma
+    def load_variables(self, saved_dict):
+        self.mean = saved_dict['mean']
+        self.var = saved_dict['var']
+        self.count = saved_dict['count']
 
-    def __call__(self, r_t, update=True):
-        if update:
-            R_t = self.gamma * self.rs.std() + r_t
-            self.rs.push(R_t)
-        return r_t / (self.rs.std() + 1e-5)
-
-    def save(self, base_path):
-        save_path = os.path.join(base_path, "reward_scaler.pth")
-        torch.save(self.rs.save_variables(), save_path)
-
-    def load(self, base_path):
-        load_path = os.path.join(base_path, "reward_scaler.pth")
-        self.rs.load_variables(torch.load(load_path))
 
 
 class ObservationNormalizer:
-    def __init__(self):
-        self.rs = RunningStat()
+    def __init__(self, num_envs, obs_shape):
+        self.num_envs = num_envs
+        self.rms = RunningMeanStd(shape=obs_shape)
 
-    def __call__(self, ob_t, update=True):
-        if update:
-            self.rs.push(ob_t)
-        return (ob_t - self.rs.mean()) / (self.rs.std() + 1e-5)
+    def __call__(self, obs):
+        self.rms.update(obs)
+        return np.clip((obs - self.rms.mean) / np.sqrt(self.rms.var + 1e-8), -10, 10)
 
     def save(self, base_path):
-        save_path = os.path.join(base_path, "observation_scaler.pth")
+        save_path = os.path.join(base_path, "observation_normalizer.pth")
         torch.save(self.rs.save_variables(), save_path)
 
     def load(self, base_path):
-        load_path = os.path.join(base_path, "observation_scaler.pth")
+        load_path = os.path.join(base_path, "observation_normalizer.pth")
         self.rs.load_variables(torch.load(load_path))
+
+
+# Reward scaler
+class RewardScaler:
+    def __init__(self, num_envs, gamma=1.):
+        self.rms = RunningMeanStd(shape=())
+        self.gamma = gamma
+        self.ret = np.zeros(num_envs)
+
+    def __call__(self, reward, dones):
+        self.ret = self.ret * self.gamma + reward
+        self.rms.update(self.ret)
+        reward = reward / np.sqrt(self.rms.var + 1e-8)
+
+        self.ret[dones] = 0
+        return np.clip(reward, -10, 10)
+
+    def save(self, base_path):
+        save_path = os.path.join(base_path, "reward_scaler.pth")
+        torch.save(self.rms.save_variables(), save_path)
+
+    def load(self, base_path):
+        load_path = os.path.join(base_path, "reward_scaler.pth")
+        self.rms.load_variables(torch.load(load_path))
+        
